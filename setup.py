2bbb6636f04b7fca7e6ceaa841ed679ba803a133
Kirill Vercetti <office@kyzima-spb.com>
2020-06-12 16:25:45 +0300

    Пример приложения

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask-adminlte-full',
-    use_scm_version=True,
+    use_scm_version={
+        'relative_to': __file__,
+    },
     url='https://github.com/kyzima-spb/flask-adminlte-full',
     description='This Flask extension is port the AdminLTE Template for easy integration into Flask Framework.',
     author='Kirill Vercetti',
     author_email='office@kyzima-spb.com',
     packages=find_packages(),
     include_package_data=True,
+    setup_requires=['setuptools_scm'],
     install_requires=[
-        'setuptools_scm>=3.5',
         'Flask>=1.0',
+        'Flask-Gravatar>=0.5',
     ],
     classifiers=[
         'Environment :: Web Environment',
